b1b04d28fed21ada3d693bc388c6aa3cc68373c2
Nick <ntomsho4545@gmail.com>
2020-04-15 23:56:01 -0400

    Initial implementation of bootstrap css

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 import SignUpForm from './signup_form';
 import SignInForm from './login_form';
@@ -9,26 +12,46 @@ const Home = (props) => {
 
     const sessionForms = () => {
         return (
-            <div>
+            <>
+            <Col>
                 <h3>Returning?</h3>
                 <SignInForm setLoggedIn={props.setLoggedInUser} />
-                <br/>
+            </Col>
+            <Col>
                 <h3>Create Account</h3>
                 <SignUpForm setLoggedIn={props.setLoggedInUser} />
-            </div>
+            </Col>
+            </>
         )
     }
 
     return (
-        <div id="home-container">
-            {props.loggedInUser ? 
-                <Link to="/ged">
-                    <button id="dnd-button">
-                        <h3>GED</h3> <br /> Guild of Expendable Dungeoneers
-                    </button>
-                </Link> :
-                sessionForms()}
-        </div>
+        <Container fluid className="full-height bg-light">
+            <Container className="py-5 justify-content-center align-items-center">
+                <Row className="justify-content-center">
+                    <h1>The DERP</h1>
+                </Row>
+                <Row className="justify-content-center">
+                    <h3 className="m-0">(Dangerously <s>Erotic</s> Eccentric Roleplaying)</h3>
+                </Row>
+                <Row className="justify-content-center">
+                    <h1>System</h1>
+                </Row>
+            </Container>
+            <Container fluid>
+                <Row>
+                    {props.loggedInUser ? 
+                        <Col>
+                            <Link to="/ged">
+                                <button className="button" id="dnd-button">
+                                    <h2>GED</h2> <br /> <h3>Guild of Expendable Dungeoneers</h3>
+                                </button>
+                            </Link>
+                        </Col> :
+                        sessionForms()}
+                </Row>
+            </Container>
+        </Container>
     )
 }
 
