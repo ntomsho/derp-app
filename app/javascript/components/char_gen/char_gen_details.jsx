29f7b45c602c7a7f2ce058674ca99d013f14bc10
Nick <ntomsho4545@gmail.com>
2020-04-18 19:34:57 -0400

    Style char_gen_details

--- app/javascript/components/char_gen/char_gen_details.jsx
+++ app/javascript/components/char_gen/char_gen_details.jsx
@@ -1,4 +1,9 @@
 import React, {useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 import { random, BACKGROUNDS, APPEARANCES, DERPS } from '../../dndb-tables';
 
 export default function CharGenDetails(props) {
@@ -14,27 +19,37 @@ export default function CharGenDetails(props) {
     }
     
     return (
-        <>
-            <div>
-                <span>Background</span>
-                <input onChange={handleChange} type="text" name="background" value={props.background} placeholder="Background"></input>
-                <button onClick={() => randomizeField('background', BACKGROUNDS)}>Reroll</button>
-                <div>What you did before adventuring</div>
-            </div>
-            <br/>
-            <div>
-                <span>Appearance</span>
-                <input onChange={handleChange} type="text" name="appearance" value={props.appearance} placeholder="Appearance"></input>
-                <button onClick={() => randomizeField('appearance', APPEARANCES)}>Reroll</button>
-                <div>What you look like</div>
-            </div>
-            <br/>
-            <div>
-                <span>Derp</span>
-                <input onChange={handleChange} type="text" name="derp" value={props.derp} placeholder="Derp"></input>
-                <button onClick={() => randomizeField('derp', DERPS)}>Reroll</button>
-                <div>Why you should never have been trusted to do this job</div>
-            </div>
-        </>
+        <Container className="mt-3">
+            <Row className="my-1">
+                <InputGroup>
+                    <InputGroup.Prepend><InputGroup.Text>Background</InputGroup.Text></InputGroup.Prepend>
+                    <Form.Control onChange={handleChange} type="text" name="background" value={props.background} placeholder="Background"></Form.Control>
+                    <InputGroup.Append>
+                        <Button variant="outline-dark" onClick={() => randomizeField('background', BACKGROUNDS)}>Reroll</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <Form.Label>What you did before adventuring</Form.Label>
+            </Row>
+            <Row className="my-1">
+                <InputGroup>
+                    <InputGroup.Prepend><InputGroup.Text>Appearance</InputGroup.Text></InputGroup.Prepend>
+                    <Form.Control onChange={handleChange} type="text" name="appearance" value={props.appearance} placeholder="Appearance"></Form.Control>
+                    <InputGroup.Append>
+                        <Button variant="outline-dark" onClick={() => randomizeField('appearance', APPEARANCES)}>Reroll</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <Form.Label>What you look like</Form.Label>
+            </Row>
+            <Row className="my-1">
+                <InputGroup>
+                    <InputGroup.Prepend><InputGroup.Text>Derp</InputGroup.Text></InputGroup.Prepend>
+                    <Form.Control onChange={handleChange} type="text" name="derp" value={props.derp} placeholder="Derp"></Form.Control>
+                    <InputGroup.Append>
+                        <Button variant="outline-dark" onClick={() => randomizeField('derp', DERPS)}>Reroll</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <Form.Label>Why you should never have been trusted to do this job</Form.Label>
+            </Row>
+        </Container>
     )
 }
