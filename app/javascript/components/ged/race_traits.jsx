12a40055668b3853e497257cb66c723d3a5aded8
Nick <ntomsho4545@gmail.com>
2020-04-20 18:49:15 -0400

    Style race_traits

--- app/javascript/components/ged/race_traits.jsx
+++ app/javascript/components/ged/race_traits.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { random, RACE_TRAITS } from '../../dndb-tables';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function RaceTraits(props) {
     
@@ -11,16 +17,32 @@ export default function RaceTraits(props) {
     
     if (props.raceString !== "Human") {
         return (
-            <div>
-                <h3>Race Traits</h3>
-                <div>
-                    <input type="text" name="0" onChange={updateTraits} value={props.raceTraits[0] || ""}></input>
-                    <button className="randomize-button" onClick={() => props.updateState('raceTraits', [random(RACE_TRAITS), props.raceTraits[1] || ""])}>🎲</button>
-                    <input type="text" name="1" onChange={updateTraits} value={props.raceTraits[1] || ""}></input>
-                    <button className="randomize-button" onClick={() => props.updateState('raceTraits', [props.raceTraits[0] || "", random(RACE_TRAITS)])}>🎲</button>
-                </div>
-                <div>You gain Magic Advantage on any rolls where either of these is helpful.</div>
-            </div>
+            <Container>
+                <Row>
+                    <h3>Race Traits</h3>
+                </Row>
+                <Row>
+                <em>You gain Magic Advantage on any rolls where either of these is helpful.</em>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col>
+                    <InputGroup>
+                        <Form.Control name="0" onChange={updateTraits} value={props.raceTraits[0] || ""}></Form.Control>
+                        <InputGroup.Append>
+                            <Button variant="dark" onClick={() => props.updateState('raceTraits', [random(RACE_TRAITS), props.raceTraits[1] || ""])}>🎲</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                    </Col>
+                    <Col>
+                    <InputGroup>
+                        <Form.Control name="1" onChange={updateTraits} value={props.raceTraits[1] || ""}></Form.Control>
+                        <InputGroup.Append>
+                            <Button variant="dark" onClick={() => props.updateState('raceTraits', [props.raceTraits[0] || "", random(RACE_TRAITS)])}>🎲</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                    </Col>
+                </Row>
+            </Container>
         )
     } else {
         return null
