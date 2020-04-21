3bd03a15c8e6660d7e426d6f4f1ef9dd3bdcaa41
Nick <ntomsho4545@gmail.com>
2020-04-21 14:46:40 -0400

    Revert to hashrouter

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
     resources :users, except: [:new, :edit, :destroy]
-    resources :ged do
-      collection do
-        resources :campaigns, except: [:new, :edit]
-        resources :campaign_subs, except: [:show, :new, :edit, :update]
-        resources :characters, except: [:new, :edit]
-        resources :invites, only: [:create, :update, :destroy]
-      end
-    end
+    resources :campaigns, except: [:new, :edit]
+    resources :campaign_subs, except: [:show, :new, :edit, :update]
+    resources :characters, except: [:new, :edit]
+    resources :invites, only: [:create, :update, :destroy]
   end
   
   root "static_pages#root"
