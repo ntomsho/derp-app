3b0e88e415c5556dfa264802aec701c9b4009d07
Nick <ntomsho4545@gmail.com>
2020-04-21 12:00:44 -0400

    Switch to BrowserRouter and nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
     resources :users, except: [:new, :edit, :destroy]
-    resources :campaigns, except: [:new, :edit]
-    resources :campaign_subs, except: [:show, :new, :edit, :update]
-    resources :characters, except: [:new, :edit]
-    resources :invites, only: [:create, :update, :destroy]
+    resources :ged do
+      collection do
+        resources :campaigns, except: [:new, :edit]
+        resources :campaign_subs, except: [:show, :new, :edit, :update]
+        resources :characters, except: [:new, :edit]
+        resources :invites, only: [:create, :update, :destroy]
+      end
+    end
   end
   
   root "static_pages#root"
 end
+# Try making another namespace for :ged with a controller that only renders an 
+# index and nesting all the resources underneath it
+
+# That fails, try cutting ged/home out of it and just making that landing page part of '/' for now
