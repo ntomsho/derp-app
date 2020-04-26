ce0a713537e46655bd17bb80ced2d61cfbba2560
Nick <ntomsho4545@gmail.com>
2020-04-26 18:09:36 -0400

    Add csrf token protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-#   protect_from_forgery with: :none
+  protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
   skip_before_action :verify_authenticity_token
   # before_action :ensure_logged_in
