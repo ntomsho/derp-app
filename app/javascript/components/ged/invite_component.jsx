0ca99b810aafc91f29f13a2d5ed43e7555906f64
Nick <ntomsho4545@gmail.com>
2020-04-26 21:20:41 -0400

    Add search spinner to invite_component

--- app/javascript/components/ged/invite_component.jsx
+++ app/javascript/components/ged/invite_component.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
 import { fetchUsers } from '../../actions/user_actions';
 
 // Selector is still messed up, fix later
@@ -9,28 +11,46 @@ import { fetchUsers } from '../../actions/user_actions';
 const InviteComponent = (props) => {
     const [query, setQuery] = useState("");
     const [usersList, setUsersList] = useState([]);
-    const [searchTimer, setSearchTimer] = useState(null);
+    const [searching, setSearching] = useState(false);
+    let timer;
 
-    useEffect(() => {
-        makeSearch();
+    useEffect((timer) => {
+        if (query === "") return setUsersList([]);
+        if (!searching) setSearching(true);
+        timer = (setTimeout(() => {
+            processSearch(timer)
+        }, 300))
     }, [query])
 
-    const makeSearch = () => {
-        clearTimeout(searchTimer)
-        setSearchTimer(setTimeout(() => {
-            query === "" ? setUsersList([]) :
-                fetchUsers({ "not_in_campaign_id": props.campaignId, "query": query }, (users) => setUsersList(users));
-        }, 300))
+    const processSearch = (timer) => {
+        fetchUsers({ "not_in_campaign_id": props.campaignId, "query": query }, (users) => createUsersList(users));
+        clearTimeout(timer);
+    }
+
+    const createUsersList = (users) => {
+        setUsersList(users);
+        setSearching(false);
+    }
+
+    const spinner = () => {
+        if (searching) {
+            return <Spinner size="sm" animation="grow" role="status" variant="dark" />
+        }
     }
 
     return (
         <Form className="w-xs-100 w-md-50 mb-4">
             <Row>
                 <Form.Label>Invite players to your campaign</Form.Label>
-                <Form.Control type="text" value={query} onChange={(e) => setQuery(e.currentTarget.value)} />
+                <InputGroup>
+                    <InputGroup.Prepend className="justify-content-center align-items-center" style={{ height: '100%', width: '6%' }}>
+                        {spinner()}
+                    </InputGroup.Prepend>
+                    <Form.Control type="text" value={query} onChange={(e) => setQuery(e.currentTarget.value)} />
+                </InputGroup>
             </Row>
             <Row>
-                <ListGroup>
+                <ListGroup className="ml-5">
                     {usersList.map(user => {
                         if (user.id !== props.loggedInUser.id && user.username.toLowerCase().startsWith(query.toLowerCase())) {
                             return (
