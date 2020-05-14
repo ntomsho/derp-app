b90ddbb1bdd14f09286b8a08b61f12d6e4fd0aa5
Nick <ntomsho4545@gmail.com>
2020-05-14 11:33:15 -0400

    Updates for websockets in heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://derp-game-app.herokuapp.com', 'http://derp-game-app.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.web_socket_server_url = "wss://derp-game-app.herokuapp.com/cable"
 end
