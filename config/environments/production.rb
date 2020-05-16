1b16c7d0937619c93ee4c0f9550ab666bd5b4ea4
Nick <ntomsho4545@gmail.com>
2020-05-16 15:09:22 -0400

    Update cable and production configs for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  congif.web_socket_server_url = "ws://derp-game-app.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'https://derp-game-app.herokuapp.com', 'http://derp-game-app.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
