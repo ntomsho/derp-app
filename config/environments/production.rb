370685aed4b29580500e27272b1903beb323ea6e
Nick <ntomsho4545@gmail.com>
2020-05-16 15:10:47 -0400

    Fix typo in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  congif.web_socket_server_url = "ws://derp-game-app.herokuapp.com/cable"
+  config.web_socket_server_url = "ws://derp-game-app.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ 'https://derp-game-app.herokuapp.com', 'http://derp-game-app.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
